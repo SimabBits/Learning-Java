c3ef120b4a0eb053ae877a631019aa3ea99e16b1
Simab <dealwithsimab@gmail.com>
2024-06-26 15:02:12 +0530

    Main.java file modified

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 public class Main {
 
-public static void main (String args []){
+    public static void main(String args[]) {
 
-int number = 22;
-char ch = 'L';
-float pointNum = 22.6f;
-double largePointNum = 22222.234224;
-long largenumber = 2343487932L;
-boolean isActive = false;
+        int number = 22;
+        char ch = 'L';
+        float pointNum = 22.6f;
+        double largePointNum = 22222.234224;
+        long largenumber = 2343487932L;
+        boolean isActive = false;
 
-System.out.println("This is int = "number"\n This is the char = " ch "\n This is the float = "pointNum "\n This is the double = "largePointNum "\n This is the long = "largenumber "\n This is the boolean = "isActive);
+        System.out.println("This is int = " + number + "\nThis is the char = " + ch + "\nThis is the float = " + pointNum + "\nThis is the double = " + largePointNum + "\nThis is the long = " + largenumber + "\nThis is the boolean = " + isActive);
 
-}
+    }
 }
