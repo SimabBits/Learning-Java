2c5c85d22e1ac171c2f9e12f7afdd95a8c1c896d
Simab <dealwithsimab@gmail.com>
2024-06-27 09:24:22 +0530

    Array.java modified

--- ArraysInJava/Array.java
+++ ArraysInJava/Array.java
@@ -17,7 +17,7 @@ Scanner sc = new Scanner(System.in);
 
 System.out.println("Enter the elements of the array");
 
-for (int i = 0; i< 5; i ++){
+for (int i = 0; i< arr.length; i ++){
 arr[i] = sc.nextInt();
 }
 
