9cc736238f75790f6c15ae3611623e496f9ad2e7
Simab <dealwithsimab@gmail.com>
2024-06-29 16:09:02 +0530

    Modified the teking input function

--- javaTakingInputs/InputFunc.java
+++ javaTakingInputs/InputFunc.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class InputFunc {
     public static void main(String args[]) {
 
-        Scanner sc = new Scanner(System.in);  
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter an Integer number: ");
         int intNum = sc.nextInt();
         System.out.println("Enter a Character: ");
-        char ch = sc.next().charAt(0);  // Fixed typo: charAT -> charAt
+        char ch = sc.next().charAt(0); // Fixed typo: charAT -> charAt
         System.out.println("Enter a floating point number: ");
         float fNum = sc.nextFloat();
         System.out.println("Enter a Long Integer number: ");
-        long longNum = sc.nextLong();  // Changed variable name to avoid conflict
+        long longNum = sc.nextLong(); // Changed variable name to avoid conflict
         System.out.println("Enter a boolean value: ");
         boolean isTrue = sc.nextBoolean();
 
@@ -22,5 +22,7 @@ public class InputFunc {
         System.out.println("Floating point number: " + fNum);
         System.out.println("Long Integer: " + longNum);
         System.out.println("Boolean: " + isTrue);
+
+        sc.close();
     }
 }
